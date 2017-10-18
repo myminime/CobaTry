4f7424d31de85fc15dce7e4acf19803bdd603e9e
sonny <sonny@OTI-NTBK-SONNY.idx.co.id>
2017-10-18 19:53:21 +0700

    coba2

--- src/Registry/GetRegistry.java
+++ src/Registry/GetRegistry.java
@@ -1,3 +1,7 @@
+package Registry;
+
+import java.awt.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,7 +16,7 @@ public class GetRegistry {
     
     public static void main(String[] args) {
  
-        // Sample usage
-        String value = WindowsRegistry.readRegistry("HKLM\\SOFTWARE\\Policies\\MyApplication\\AES", "SecurityKey");
-        System.out.println(value);
+        List<String> ls = WinRegistry.readStringSubKeys(WinRegistry.HKEY_LOCAL_MACHINE,"SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\");
+        String key = ls.stream().filter(st -> st.matches("Maxima.*")).findAny().get();
+    }
 }
